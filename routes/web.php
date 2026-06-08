7709de546dd77356f72c59e2c1eff3440a64db9d
MichaelDzaychiks <122859459+MichaelDzaychiks@users.noreply.github.com>
2026-06-08 15:07:53 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/tentang', function() {
